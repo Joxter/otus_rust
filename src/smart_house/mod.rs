09091579aab943b0f8e653ae02d208d1374d17a8
Nikolai Morozov <nmorozov@apcreation.nl>
2022-06-03 13:51:52 +0200

    naive tests

--- src/smart_house/mod.rs
+++ src/smart_house/mod.rs
@@ -33,7 +33,10 @@ impl SmartHouse {
     }
 
     pub fn get_rooms(&self) -> Vec<String> {
-        self.rooms.clone().into_keys().collect()
+        let mut keys: Vec<String> = self.rooms.clone().into_keys().collect();
+        // сортировка только для стабильности результата
+        keys.sort();
+        keys
     }
 
     pub fn get_devices(&self, room: &str) -> Vec<String> {
@@ -56,5 +59,70 @@ impl SmartHouse {
         }
 
         report
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::smart_house::{DeviceInfoProvider, Report, SmartHouse};
+    use std::collections::HashMap;
+    // use crate::SmartHouse;
+
+    pub struct TestDevice {
+        name: String,
+    }
+
+    impl Report for TestDevice {
+        fn get_report(&self) -> String {
+            format!("report {}", self.name)
+        }
+    }
+
+    pub struct TestProvider<'a> {
+        items: HashMap<String, &'a dyn Report>,
+    }
+
+    impl<'a> TestProvider<'a> {
+        pub fn new() -> Self {
+            Self {
+                items: HashMap::new(),
+            }
+        }
+    }
+
+    impl<'a> DeviceInfoProvider<'a> for TestProvider<'a> {
+        fn get_report(&self, room: &str, name: &str) -> String {
+            match self.items.get(&format!("{}_{}", room, name)) {
+                Some(device) => device.get_report(),
+                None => format!("{:} is not found", name),
+            }
+        }
+
+        fn add_device(&mut self, room: &str, name: &str, device: &'a dyn Report) {
+            self.items.insert(format!("{}_{}", room, name), device);
+        }
+    }
+
+    #[test]
+    fn it_works() {
+        let mut house = SmartHouse::new("my house");
+
+        house.add_room("kitchen");
+        house.add_device("kitchen", "thermo_1");
+        house.add_device("kitchen", "thermo_2");
+        house.add_room("bedroom");
+
+        assert_eq!(vec!["bedroom", "kitchen"], house.get_rooms());
+        assert_eq!(vec!["thermo_1", "thermo_2"], house.get_devices("kitchen"));
+        let empty_string_vec: Vec<String> = Vec::new();
+        assert_eq!(empty_string_vec, house.get_devices("bedroom"));
+
+        let thermo1 = TestDevice {
+            name: "thermo_1".to_string(),
+        };
+        let mut info_provider_1 = TestProvider::new();
+        info_provider_1.add_device("kitchen", "thermo_1", &thermo1);
+
+        assert_eq!("### House \"my house\"\nbedroom:\nkitchen:\n> report thermo_1\n> thermo_2 is not found\n", house.create_report(&info_provider_1));
     }
 }
