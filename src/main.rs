bb83c6122ddb74b795ee13344f8e1109318eaf35
Nikolai Morozov <nmorozov@apcreation.nl>
2022-06-03 09:45:54 +0200

    SmartHouse2 basic

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@
  */
 
 use crate::device_provider::DeviceInfoProvider;
-use crate::smart_house::SmartHouse;
+use crate::smart_house::{SmartHouse, SmartHouse2};
 
 mod smart_house;
 mod device_provider;
@@ -57,31 +57,68 @@ impl DeviceInfoProvider for BorrowingDeviceInfoProvider<'_, '_> {
 }
 
 fn main() {
-    let house = SmartHouse {
-        name: "my house".to_string(),
-        kitchen: ["socket_1", "thermo_1", "thermo_2"],
-        bedroom: ["socket_2", "socket_3", "thermo_3"],
-    };
+
+    let mut house = SmartHouse2::new("my house");
+
+    house.add_room("kitchen");
+    house.add_device("kitchen", "socket_1");
+    house.add_device("kitchen", "thermo_1");
+    house.add_room("bedroom");
+    house.add_device("bedroom", "thermo_1");
+
+    println!("get_rooms: {:?}", house.get_rooms());
+    println!("get_devices(kitchen): {:?}", house.get_devices("kitchen"));
+    println!("get_devices(bedroom): {:?}", house.get_devices("bedroom"));
 
     let socket1 = SmartSocket {
         name: "socket_1".to_string(),
     };
     let info_provider_1 = OwningDeviceInfoProvider { socket: socket1 };
 
+    println!("report: \n{}", house.create_report(&info_provider_1));
+
+    /*
+
+    house.create_room("bedroom");
+    house.add_device("kitchen", "socket_1");
+    house.add_device("kitchen", "socket_2");
+    house.add_device("kitchen", "thermo_1");
+
+    let provider_1 = OwningDeviceInfoProvider::new();
+    provider_1.add_device("kitchen", "socket_1", socket1)
+    provider_1.add_device("kitchen", "thermo_1", thermo1)
+
     let report1 = house.create_report(&info_provider_1);
     println!("Report #1: {report1}");
 
-    let socket2 = SmartSocket {
-        name: "socket_2".to_string(),
-    };
-    let thermo3 = SmartThermometer {
-        name: "thermo_3".to_string(),
-    };
-    let info_provider_2 = BorrowingDeviceInfoProvider {
-        socket: &socket2,
-        thermo: &thermo3,
-    };
+    */
 
-    let report2 = house.create_report(&info_provider_2);
-    println!("\nReport #2: {report2}");
+
+    // let house = SmartHouse {
+    //     name: "my house".to_string(),
+    //     kitchen: ["socket_1", "thermo_1", "thermo_2"],
+    //     bedroom: ["socket_2", "socket_3", "thermo_3"],
+    // };
+    //
+    // let socket1 = SmartSocket {
+    //     name: "socket_1".to_string(),
+    // };
+    // let info_provider_1 = OwningDeviceInfoProvider { socket: socket1 };
+    //
+    // let report1 = house.create_report(&info_provider_1);
+    // println!("Report #1: {report1}");
+    //
+    // let socket2 = SmartSocket {
+    //     name: "socket_2".to_string(),
+    // };
+    // let thermo3 = SmartThermometer {
+    //     name: "thermo_3".to_string(),
+    // };
+    // let info_provider_2 = BorrowingDeviceInfoProvider {
+    //     socket: &socket2,
+    //     thermo: &thermo3,
+    // };
+    //
+    // let report2 = house.create_report(&info_provider_2);
+    // println!("\nReport #2: {report2}");
 }
--- src/smart_house/mod.rs
+++ src/smart_house/mod.rs
@@ -1,4 +1,5 @@
 use crate::DeviceInfoProvider;
+use std::collections::HashMap;
 
 pub struct SmartHouse<'a, 'b> {
     pub name: String,
@@ -43,3 +44,52 @@ impl SmartHouse<'static, 'static> {
     }
 }
 
+pub struct SmartHouse2 {
+    name: String,
+    rooms: HashMap<String, Vec<String>>,
+}
+
+impl SmartHouse2 {
+    pub fn new(name: &str) -> Self {
+        Self {
+            name: name.to_string(),
+            rooms: HashMap::new(),
+        }
+    }
+
+    pub fn add_room(&mut self, name: &str) {
+        self.rooms.insert(name.to_string(), vec![]);
+    }
+
+    pub fn add_device(&mut self, room: &str, device: &str) {
+        if let Some(r) = self.rooms.get_mut(room) {
+            r.push(device.to_string())
+        };
+    }
+
+    pub fn get_rooms(&self) -> Vec<String> {
+        self.rooms.clone().into_keys().collect()
+    }
+
+    pub fn get_devices(&self, room: &str) -> Vec<String> {
+        if let Some(r) = self.rooms.get(room) {
+            return r.clone();
+        }
+        vec![]
+    }
+
+    pub fn create_report<T: DeviceInfoProvider>(&self, provider: &T) -> String {
+        let mut report = format!("### House \"{}\"\n", self.name).clone();
+
+        for room in self.get_rooms() {
+            report.push_str(format!("{}:\n", room).as_str());
+
+            for device in self.get_devices(room.as_str()) {
+                let device_report = provider.get_info(room.as_str(), device.as_str());
+                report.push_str(format!("> {}\n", device_report).as_str());
+            }
+        }
+
+        report
+    }
+}
