d1c1c427930a3fc1769be80831169cd1181f8e01
Nikolai Morozov <nmorozov@apcreation.nl>
2022-06-03 15:55:45 +0200

    add some SmartSocket methods

--- src/devices.rs
+++ src/devices.rs
@@ -16,14 +16,42 @@ impl SmartSocket {
             power,
         }
     }
+
+    #[allow(dead_code)]
+    fn turn_on(&mut self) {
+        self.is_on = true
+    }
+
+    #[allow(dead_code)]
+    fn turn_off(&mut self) {
+        self.is_on = false
+    }
+
+    fn get_descr(&self) -> String {
+        self.descr.clone()
+    }
+
+    fn get_status(&self) -> String {
+        if self.is_on {
+            "ON".to_string()
+        } else {
+            "OFF".to_string()
+        }
+    }
+
+    fn get_power(&self) -> u32 {
+        self.power
+    }
 }
 
 impl Report for SmartSocket {
     fn get_report(&self) -> String {
-        let status = if self.is_on { "ON" } else { "OFF" };
         format!(
             "Smart socket {}: \"{}\" ({}/{}W)",
-            self.name, self.descr, status, self.power
+            self.name,
+            self.get_descr(),
+            self.get_status(),
+            self.get_power()
         )
     }
 }
